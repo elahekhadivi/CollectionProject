088f1e821692a42f59256db751ab0fc0200ebfc6
elahe_khavidi <elahe.khavidi@akerna.com>
2022-04-25 20:03:33 -0400

    first commit

---
+++ GenericProject/Program.cs
@@ -0,0 +1,19 @@
+﻿public class GenericList<T> where T : struct
+{
+	
+    private List<T> listotT = new List<T>();
+    public void Add(T item, List<T> listofT)
+    {
+        listofT.Add(item);
+
+    }
+    public T returnItem (int itemIndex)
+    {
+        return listotT[itemIndex];
+    }
+    public List<T> returnSorted(List<T> listofT)
+    {
+        return listotT.OrderByDescending(o => o);
+    }
+
+}
